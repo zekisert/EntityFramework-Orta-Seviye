a0f6043ac37e61d4fcf12236849bb14fbe472714
zekisert <49846775+zekisert@users.noreply.github.com>
2020-03-07 22:31:29 +0300

    UrunContext.cs Context
    Context Class

---
+++ UrunContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkSamples
+{
+    public class UrunContext : DbContext
+    {
+        public UrunContext() : base("urunConnection")
+        {
+
+        }
+        public DbSet<Kategori> Kategoriler { get; set; }
+        public DbSet<Urun> Urunler { get; set; }
+
+    }
+}
